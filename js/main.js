618b6168fb58705376dd73356038fac7c50eaeb9
east32 <46858464+east32@users.noreply.github.com>
2021-01-30 18:11:56 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@ const getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const  getMaxStringValue = function (str, max)  {
-  if(str.length > max)  {
-    alert(`You cannot post comments over ${max} symbols `);}}
+const  checkMaxStringValue = function (str, max)  {
+  return str.length <= max ? true : false;
+};
 
-let comment = 'В этом комментарии должно быть около 140 символов, а  на самом деле должно быть больше, чтобы проверить работоспособность функции... 140 символов оказывается довольно большой текст и нужно что то придумывать'
-
-
-
-getMaxStringValue(comment, 140);
+const comment = 'В этом комментарии должно быть около 140 символов, а  на самом деле должно быть больше, чтобы проверить работоспособность функции... 140 символов оказывается довольно большой текст и нужно что то придумывать'
+const secondComment = 'В этом комментарии меньше 140 символов ';
+checkMaxStringValue(comment, 140);
+checkMaxStringValue(secondComment, 140);
 getRandomInt(0, 1);
