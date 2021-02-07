b590779ebb9154ee6d721fad4fb75d83e2a3c51a
east32 <46858464+east32@users.noreply.github.com>
2021-02-07 09:03:06 +0300

    Генерация данных

---
+++ js/main.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const SIMILLAR_OBJECTS_AMOUNT = 25;
+
+const COMMENTATOR_NAMES = [
+  'Илья',
+  'Вика',
+  'Фёдор',
+  'Настя',
+  'Алексей',
+  'Жанна',
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCPRIPTION = [
+  'Картинка',
+  'Красивая картнка',
+  'Очень красивая картинка',
+  'Классно',
+  'Хорошо',
+];
+
+const getRandomInt = function (min, max) {
+
+  if (min < 0 || max < 0) {
+    return
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+const  checkMaxStringValue = function (str, max)  {
+  return str.length <= max ? true : false;
+};
+
+checkMaxStringValue ('asdasdasd', 5);
+
+
+const getRandomArrayEl = function(arr) {
+  return arr[getRandomInt(0, arr.length - 1)];
+};
+
+const getAvatarPath = function () {
+  return `img/avatar-${getRandomInt(1, 6)}.svg`
+};
+
+const getUniqId = function () {
+  const idArray = [];
+  for(let i = 0;i < 25; i++) {
+    idArray.push(i);
+  }
+  return getRandomArrayEl(idArray);
+
+};
+
+const getActualComments = function () {
+  const actualCommentsStrings = [];
+  for(let i = 0;i < getRandomInt(1, 2); i++ ) {
+    actualCommentsStrings.push(COMMENTS[getRandomInt(0, COMMENTS.length-1)])}
+  return actualCommentsStrings;
+};
+
+const generateRandomComment = function () {
+  const commentsArray = [];
+  const commentsCounter = getRandomInt(1, 6);
+  for (let i = 0; i < commentsCounter; i++) {
+    const newComment = {
+      id: getUniqId(),
+      avatar: getAvatarPath(),
+      message: getActualComments(),
+      name : getRandomArrayEl(COMMENTATOR_NAMES),
+    };
+    commentsArray.push(newComment);
+  }
+  return commentsArray;
+};
+
+const generateObject = function () {
+  const objectsArray = [];
+  for (let i = 0; i < SIMILLAR_OBJECTS_AMOUNT; i++) {
+    const newObject = {
+      id: getUniqId(),
+      url: `photos/${getUniqId()}.jpg`,
+      description: getRandomArrayEl(DESCPRIPTION),
+      likes: getRandomInt(15, 200),
+      comments: generateRandomComment(),
+    };
+    objectsArray.push(newObject);
+  }
+  return objectsArray;
+}
+
+generateObject();
